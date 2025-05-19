eb6d91b7455af59ce90e26e0232467ad7f596e9a
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2025-05-19 13:22:17 +0200

    Check for 'SqsManagedSseEnabled' in CKV AWS 27

--- checkov/cloudformation/checks/resource/aws/SQSQueueEncryption.py
+++ checkov/cloudformation/checks/resource/aws/SQSQueueEncryption.py
@@ -1,11 +1,10 @@
 from typing import Any
 
-from checkov.common.models.enums import CheckCategories
-from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
-from checkov.common.models.consts import ANY_VALUE
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceCheck
 
 
-class SQSQueueEncryption(BaseResourceValueCheck):
+class SQSQueueEncryption(BaseResourceCheck):
     def __init__(self) -> None:
         name = "Ensure all data stored in the SQS queue is encrypted"
         id = "CKV_AWS_27"
@@ -13,11 +12,13 @@ class SQSQueueEncryption(BaseResourceValueCheck):
         categories = [CheckCategories.ENCRYPTION]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self) -> str:
-        return 'Properties/KmsMasterKeyId'
-
-    def get_expected_value(self) -> Any:
-        return ANY_VALUE
+    def scan_resource_conf(self, conf: dict[str, list[Any]]) -> CheckResult:
+        if 'Properties' in conf and 'KmsMasterKeyId' in conf['Properties']:
+            return CheckResult.PASSED
+        if 'Properties' in conf and 'SqsManagedSseEnabled' in conf['Properties']:
+            if conf['Properties']['SqsManagedSseEnabled'] == True:
+                return CheckResult.PASSED
+        return CheckResult.FAILED
 
 
 check = SQSQueueEncryption()
