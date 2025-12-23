7761ba2e3df16f14d003c2ea009509911b50d844
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2025-12-23 19:10:26 +0100

    Do not flag CKV_AZURE_59 if network ACL is set

--- checkov/arm/checks/resource/StorageAccountDisablePublicAccess.py
+++ checkov/arm/checks/resource/StorageAccountDisablePublicAccess.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 
 from typing import Any
 
-from checkov.arm.base_resource_negative_value_check import BaseResourceNegativeValueCheck
-from checkov.common.models.enums import CheckCategories
+from checkov.arm.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.common.util.type_forcers import force_int
 
 
-class StorageAccountDisablePublicAccess(BaseResourceNegativeValueCheck):
+class StorageAccountDisablePublicAccess(BaseResourceCheck):
     def __init__(self) -> None:
         name = "Ensure that Storage accounts disallow public access"
         id = "CKV_AZURE_59"
@@ -14,11 +15,32 @@ class StorageAccountDisablePublicAccess(BaseResourceNegativeValueCheck):
         categories = (CheckCategories.NETWORKING,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def get_inspected_key(self) -> str:
-        return "properties/publicNetworkAccess"
+    def scan_resource_conf(self, conf):
+        if "apiVersion" in conf:
+            # Fail if apiVersion < 2017 as you could not set networkAcls
+            year = force_int(conf["apiVersion"][0:4])
 
-    def get_forbidden_values(self) -> list[Any]:
-        return ["Enabled"]
+            if year is None:
+                return CheckResult.UNKNOWN
+            elif year < 2017:
+                return CheckResult.FAILED
+
+        if "properties" in conf:
+            # Default value is 'Enabled', so let's check if the user explictly disabled it.
+            if "publicNetworkAccess" in conf["properties"]:
+                if conf["properties"]["publicNetworkAccess"] == "Disabled":
+                    return CheckResult.PASSED
+
+            if "networkAcls" in conf["properties"]:
+                if not isinstance(conf["properties"]["networkAcls"], dict):
+                    return CheckResult.FAILED
+                
+                # The endpoint is still reachable from public internet (as in, responds to DNS queries), however, access is disabled by the firewall.
+                if "defaultAction" in conf["properties"]["networkAcls"]:
+                    if conf["properties"]["networkAcls"]["defaultAction"] == "Deny":
+                        return CheckResult.PASSED
+                    
+        return CheckResult.PASSED
 
 
 check = StorageAccountDisablePublicAccess()
